140b6d05fe043123a36210d852d20185edb00bcc
Bouloute <annaick1995@gmail.com>
2020-11-18 15:57:31 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,41 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
+//https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            reviews: []
+        }
+    }
+    
+    componentDidMount() {
+        this.fetchReviews()
+    }
+    
+    fetchReviews = () => {
+        fetch(URL)
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({
+                reviews: data.results
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="latest-movie-reviews">
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        );
+    }
+}
+//The LatestMovieReviewsContainer should have a top-level wrapping element with class latest-movie-reviews.
+export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,47 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            reviews: [],
+            searchTerm: ''
+        }
+    }
+        
+    handleTyping = (event) => {
+        this.setState({ searchTerm: event.target.value })
+    }
+
+    search = (event) => {
+        event.preventDefault()
+
+        this.fetchReviews()
+    }
+
+    fetchReviews = () => {
+        fetch(URL + `&query=${this.state.searchTerm}`)
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({
+                reviews: data.results
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+                <form onSubmit={this.search}>
+                    <input type="text" onChange={this.handleTyping}></input>
+                    <input type="submit" value="Search"></input>
+                </form>
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        );
+    }
+}
+// The SearchableMovieReviewsContainer should have a top-level wrapping element with class searchable-movie-reviews
+export default SearchableMovieReviewsContainer;
